2b1af7919fcde2648f2678f9d9858c3cc20b2f82
Kelly Stoops <kstoops815@gmail.com>
2017-09-02 10:23:13 -0500

    learning es6

--- es6Lesson.js
+++ es6Lesson.js
@@ -2,10 +2,17 @@
 //Fat arrow functions
 //Object literal value shorthand notation
 //String template literals
+//let poop = "crap"; //let allows you to change the value to the variable
+//poop = 4;
 
-var allProducts = [];
 
-var product1 = {
+//const does not allow you to change the value of the variable
+//you never declare a const with no value it would never have a value
+//const poop = "crap"; 
+
+let allProducts = [];
+
+const product1 = {
   name: "Mop Attire",
   imagePath: "./images/mopAttire.jpg",
   imageAlt: "Product: Mop Attire",
@@ -14,7 +21,9 @@ var product1 = {
   soldOut: false
 };
 
-var product2 = {
+//you can change things within the object, just not the object itself using a const
+
+const product2 = {
   name: "Taco Suit",
   imagePath: "./images/tacoSuit.jpg",
   imageAlt: "Product: Taco Suit",
@@ -23,7 +32,7 @@ var product2 = {
   soldOut: false
 };
 
-var product3 = {
+const product3 = {
   name: "Neck Decoration",
   imagePath: "./images/neckDecoration.jpg",
   imageAlt: "Product: Neck Decoration",
@@ -32,7 +41,7 @@ var product3 = {
   soldOut: false
 };
 
-var product4 = {
+const product4 = {
   name: "Head Ornament",
   imagePath: "./images/headOrnament.jpg",
   imageAlt: "Product: Head Ornament",
@@ -41,7 +50,7 @@ var product4 = {
   soldOut: true
 };
 
-var product5 = {
+const product5 = {
   name: "Boob Hat",
   imagePath: "./images/boobHat.jpg",
   imageAlt: "Product: Boob Hat",
@@ -56,15 +65,29 @@ allProducts.push(product3);
 allProducts.push(product4);
 allProducts.push(product5);
 
+
+//Object literal value shorthand notation
+const addNewProduct = (name, imagePath, imageAlt, description, price, soldOut) => {
+  const newProduct = {name, imagePath, imageAlt, description, price, soldOut};
+  allProducts.push(newProduct);
+}
+
+addNewProduct("Butt Cream", "https://upload.wikimedia.org/wikipedia/en/e/e8/Boudreauxs_Butt_Paste.jpg", "Here is a picture of butt cream", "rub on", 4.99, true);
+
 console.log("All my weird baby products", allProducts);
 
-var productContainer = document.getElementById("product-container");
 
 
 
 
-function buildDomString (product){
-  var domString = "";
+const productContainer = document.getElementById("product-container");
+
+
+
+
+const buildDomString = (product) => {
+  //var changed to let because you are always changing the value
+  let domString = "";
 
 
   domString+=   '<section class="product">';
@@ -88,26 +111,34 @@ function buildDomString (product){
 
 }
 
-var printProductArrayToDom = function(productArray){
-  for(var i = 0; i < productArray.length; i++) {
-
-  var currentProduct = productArray[i];
-  var productDomString = buildDomString(currentProduct);
+//change the var to const so that you can't overwrite the variable later 
+//fat arrow function notation
+const printProductArrayToDom = (productArray) => {
+  //the var has to be let since the value is always changing thru the for loop
+  for(let i = 0; i < productArray.length; i++) {
+//these should be changed to const since they don't exist outside of the for loop
+//also, once you go thru the for loop once, then it is erased and goes thru the 
+//loop the next time, and it only knows that value, and so on and so forth
+  const currentProduct = productArray[i];
+  const productDomString = buildDomString(currentProduct);
   productContainer.innerHTML += productDomString;
   }
 }
 
 printProductArrayToDom(allProducts);
 
-var selectedCard;
 
-document.getElementById("product-container").addEventListener("click", function(event){
+//this var changes to let since the value is always changing
+let selectedCard;
+
+//inside the event listener, an anonymous function using fat arrow function
+document.getElementById("product-container").addEventListener("click", (event) => {
   console.log(event);
   changeBorder(event);
   printSelectedDescription();
 });
 
-function changeBorder(event){
+const changeBorder = (event) => {
   if (event.target.classList.contains("child")){
     selectedCard = event.target.parentNode;
   } else if (event.target.parentNode.parentNode.classList.contains("product")){
@@ -119,7 +150,10 @@ function changeBorder(event){
   selectedCard.classList.add("border-funsies");
 }
 
-function printSelectedDescription(){
-  var description = selectedCard.childNodes[2].childNodes[0].innerHTML;
+
+
+const printSelectedDescription = () => {
+  //this var changes to const since it is inside a function and not global
+  const description = selectedCard.childNodes[2].childNodes[0].innerHTML;
   console.log(description);
 }
